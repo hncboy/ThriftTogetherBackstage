4da69040498d3decb2fe482ec5a0cde251afb174
hncboy <619452863@qq.com>
2019-05-13 13:36:22 +0800

    搭建了基础框架

---
+++ src/test/java/com/pro516/thrifttogetherbackstage/ThriftTogetherBackstageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pro516.thrifttogetherbackstage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThriftTogetherBackstageApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
